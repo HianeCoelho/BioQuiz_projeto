c472f7c30504f16e981edfcd7261ba463ac6fbf3
HianeCoelho <137707058+HianeCoelho@users.noreply.github.com>
2023-11-22 19:48:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-let quest = Array(15).fill(0);
+let quest = 0;
 let num = 0;
 
-function displayArray() {
-    let arrayDisplayDiv = document.getElementById('arrayDisplay');
-    let arrayString = quest.join(', ');
-    arrayDisplayDiv.innerHTML = arrayString;
-}
-displayArray();
-
 function correct() {
-    quest[num] = 1;
-    displayArray();
+    quest += 1;
+    console.log('Value of quest: ' + quest);
 }
 
-function incrementNum() {
-    num++;
-    console.log('Value of num: ' + num);
-}
+var g_iCount = 30;
 
+function contagem(){
+    var prg = document.getElementById('progresso-da-barra');
+    var mostraContagem = document.getElementById('mostracontagem');
+
+    if(g_iCount >= 1){
+        mostraContagem.innerText = g_iCount;
+    
+        var tempo_restante = (g_iCount / 30) * 100;
+        
+        prg.style.width = tempo_restante + '%';
+
+        if(g_iCount <= 20 && g_iCount > 10){
+            prg.style.backgroundColor = "#f8ca3f"
+        }
+        else if(g_iCount <= 10){
+            prg.style.backgroundColor = "#d75151"
+        }
+
+        g_iCount--;
+    
+        setTimeout(contagem, 1000);
+    }
+    else{
+        window.location.href = '#.html';  // colocar a página de explicação da resposta aqui 
+    }
+}
